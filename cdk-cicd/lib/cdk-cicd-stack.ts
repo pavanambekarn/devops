e2453866071cc8c0e2c084ec446339641c8d0b83
pavanambekarn <pavanhimalaya820@gmail.com>
2024-11-29 11:32:13 +0530

    cdk

---
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { Construct } from 'constructs';
+
+
+export class CdkCicdStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // new CodePipeline(this, 'cicd', {
+    //     pipelineName: 'cicd',
+    //     synth: new ShellStep('Synt', {
+    //       input: CodePipelineSource.gitHub('pavanambekarn/devops', 'cicd-practice'),
+    //       commands: [
+    //         'cd cdk-cicd',
+    //         'npm ci',
+    //         'npx cdk synth'
+    //       ],
+    //       primaryOutputDirectory: 'cdk-cicd/cdk.out'
+    //     } )
+    // });
+  }
+}
