d22abf9ce69f6e6870beefec0f284cf5f72032b1
pavanambekarn <pavanhimalaya820@gmail.com>
2024-11-29 17:47:06 +0530

    cdk1

--- cdk-cicd/bin/cdk-cicd.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { CdkCicdStack } from '../lib/cdk-cicd-stack';
-
-const app = new cdk.App();
-new CdkCicdStack(app, 'CdkCicdStack', {
-});
