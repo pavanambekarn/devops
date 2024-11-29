5e6b572d6347a0cac5ace2acf6cc0c4a4277665f
pavanambekarn <pavanhimalaya820@gmail.com>
2024-11-29 12:20:07 +0530

    cdk

--- cdk-cicd/lib/cdk-cicd-stack.ts
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -7,17 +7,17 @@ export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // new CodePipeline(this, 'cicd', {
-    //     pipelineName: 'cicd',
-    //     synth: new ShellStep('Synt', {
-    //       input: CodePipelineSource.gitHub('pavanambekarn/devops', 'cicd-practice'),
-    //       commands: [
-    //         'cd cdk-cicd',
-    //         'npm ci',
-    //         'npx cdk synth'
-    //       ],
-    //       primaryOutputDirectory: 'cdk-cicd/cdk.out'
-    //     } )
-    // });
+    new CodePipeline(this, 'cicd', {
+        pipelineName: 'cicd',
+        synth: new ShellStep('Synt', {
+          input: CodePipelineSource.gitHub('pavanambekarn/devops', 'cicd-practice'),
+          commands: [
+            'cd cdk-cicd',
+            'npm ci',
+            'npx cdk synth'
+          ],
+          primaryOutputDirectory: 'cdk-cicd/cdk.out'
+        } )
+    });
   }
 }
