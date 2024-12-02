867381b5d7557e372200d39b38afc2a1fffa14d7
pavanambekarn <pavanhimalaya820@gmail.com>
2024-12-02 08:45:28 +0530

    cdk

--- cicd/lib/cdk-cicd-stack.ts
+++ cicd/lib/cdk-cicd-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 
@@ -6,5 +7,20 @@ export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+
+    new CodePipeline(this, 'Pipelin', {
+      pipelineName: 'Pipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('pavanambekarn/devops', 'cdk', {
+          authentication: cdk.SecretValue.secretsManager('cicd'),
+        }),
+        commands: [
+          'cd cicd',
+          'npm ci',
+          'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'cicd/cdk.out'
+      })
+    })
   }
 }
