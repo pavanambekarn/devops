749fc04d403da7397e73573701be7e98b2db5a1c
pavanambekarn <pavanhimalaya820@gmail.com>
2024-12-02 09:50:03 +0530

    teststage

--- cicd/lib/cdk-cicd-stack.ts
+++ cicd/lib/cdk-cicd-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { PipelineStage } from './PipelineStage';
 
@@ -26,6 +26,14 @@ export class CdkCicdStack extends cdk.Stack {
 
     const testStage = pipeline.addStage(new PipelineStage(this, 'PipelineTestStage', {
        stageName: 'test'
+    }));
+
+    testStage.addPre(new CodeBuildStep('unit-test', {
+      commands: [
+        'cd cicd',
+        'npm ci',
+        'npm test'
+      ]
     }))
   }
 }
--- cicd/test/cicd.test.ts
+++ cicd/test/cicd.test.ts
@@ -1,17 +1,9 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cicd from '../lib/cicd-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cicd-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Cicd.CicdStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
-});
+
+
+describe('Hello describe test suite', () => {
+    test('handler should return 200', async () => {
+       return 'hi'
+    }) 
+})
